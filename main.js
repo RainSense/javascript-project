61086c1c348a079d4413676b9c190211f7d8196c
Lee <jjisanle@gmail.com>
2020-12-13 13:05:08 +0900

    this is going to be good

---
+++ main.js
@@ -0,0 +1,17 @@
+//Exercise - extend the functionality of a built in object
+
+//#1
+//Date object => to have new method .lastYear()
+//whitch shows you last year 'YYYY' format.
+
+let date = new Date('1900-10-10').getFullYear();
+//'1800'
+
+Function.__ptoro__
+
+
+
+//#Bonus
+//Modify .map() to print ''
+
+
